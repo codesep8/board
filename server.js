59a433e8cf0acca7bda6cc6433279a501cbd1f6b
Junyoung Kim <codesep8@gmail.com>
2025-01-19 16:10:08 +0000

    express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const compression = require("compression");
+
+const app = express();
+app.use('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(compression());
+
+app.listen(3000, () => {
+    console.log("http://localhost:3000")
+});
