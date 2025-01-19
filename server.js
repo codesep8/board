bc8854ca6d56088f9324b1a089f6ff71683afd0c
Junyoung Kim <codesep8@gmail.com>
2025-01-19 16:13:08 +0000

    for secure

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const compression = require("compression");
+const helmet = require("helmet");
 
 const app = express();
 app.use('view engine', 'ejs');
 app.use(expressLayouts);
 app.use(compression());
+app.use(helmet());
 
 app.listen(3000, () => {
     console.log("http://localhost:3000")
